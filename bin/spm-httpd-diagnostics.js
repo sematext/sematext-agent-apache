d9bab945e707c837fa1ae3feee293aae17098f9e
Adnan Rahic <ado.raha198@gmail.com>
2020-05-10 11:26:31 +0200

    update diag package

--- bin/spm-httpd-diagnostics.js
+++ bin/spm-httpd-diagnostics.js
@@ -1,19 +1,19 @@
-#!/bin/sh
-':' // ; export MAX_MEM="--max-old-space-size=200"; exec "$(command -v node || command -v nodejs)" "${NODE_OPTIONS:-$MAX_MEM}" "$0" "$@" 
-
+#!/usr/bin/env node
 /*
  * @copyright Copyright (c) Sematext Group, Inc. - All Rights Reserved
  *
- * @licence SPM for Apache is free-to-use, proprietary software.
+ * @licence SPM for Apache2/httpd is free-to-use, proprietary software.
  * THIS IS PROPRIETARY SOURCE CODE OF Sematext Group, Inc. (Sematext)
  * This source code may not be copied, reverse engineered, or altered for any purpose.
  * This source code is to be used exclusively by users and customers of Sematext.
  * Please see the full license (found in LICENSE in this distribution) for details on its license and the licenses of its dependencies.
  */
-var ZipZipTop = require('zip-zip-top')
-var zip = new ZipZipTop()
+var fs = require('fs')
+var AdmZip = require('adm-zip')
+var zip = new AdmZip()
 var config = require('spm-agent').Config
 var util = require('util')
+var ls = require('ls')
 var os = require('os')
 var path = require('path')
 
@@ -23,23 +23,48 @@ var systemInfo = {
   processEnvironment: process.env
 }
 
-zip.file('systemInfo.txt', util.inspect(config).toString() + '\nSystem-Info:\n' + util.inspect(systemInfo))
-zip.zipFolder(config.logger.dir, function (err, data) {
-  if (err) {
-    return console.error(err)
-  }
-  zip.zipFolder('/etc/sematext', function (err, data) {
-    if (err) {
-      return console.error(err)
+// load ENV like Logsene receivers from file containing
+// env vars e.g. SPM_RECEIVER_URL, EVENTS_RECEIVER_URL, LOGSENE_RECEIVER_URL
+// the file overwrites the actual environment
+// and is used by Sematext Enterprise or multi-region setups to
+// setup receiver URLs
+function loadEnvFromFile (fileName) {
+  try {
+    var receivers = fs.readFileSync(fileName).toString()
+    if (receivers) {
+      var lines = receivers.split('\n')
     }
-    var archFileName = path.join(os.tmpdir(), 'spm-diagnose.zip')
-    zip.writeToFile(archFileName, (err) => {
-      if (err) {
-        console.error(err)
+    systemInfo.receiverConfigFileUsed = fileName
+    systemInfo.receivers = receivers
+    console.log('Reading receivers.config: ' + fileName)
+    lines.forEach(function (line) {
+      var kv = line.split('=')
+      if (kv.length === 2 && kv[1].length > 0) {
+        process.env[kv[0].trim()] = kv[1].trim()
+        console.log(kv[0].trim() + ' = ' + kv[1].trim())
       }
     })
-    console.log('Sematext diagnostics info is in  ' + archFileName)
-    console.log('Please e-mail the file to support@sematext.com')
-  })
+  } catch (error) {}
+}
+var envFileName = '/etc/sematext/receivers.config'
+/**
+  if (/win/.test(os.platform()) {
+    envFileName = process.env.ProgramData + '\\Sematext\\receivers.config'
+  }
+**/
+loadEnvFromFile(envFileName)
+
+var cfgDumpFileName = path.join(os.tmpdir(), 'sematext-agent-httpd-diagnostics.json')
+fs.writeFileSync(cfgDumpFileName, util.inspect(config).toString() + '\nSystem-Info:\n' + util.inspect(systemInfo))
+var logfiles = ls(config.logger.dir + '/*')
+zip.addLocalFile(cfgDumpFileName)
+
+logfiles.forEach(function (f) {
+  zip.addLocalFile(f.full)
 })
 
+var archFileName = path.join(os.tmpdir(), 'sematext-agent-httpd-diagnostics.zip')
+zip.writeZip(archFileName)
+console.log('Sematext Agent Httpd diagnostics info is in:  ' + archFileName)
+console.log('Please e-mail the file to support@sematext.com')
+fs.unlink(cfgDumpFileName, function () {})
